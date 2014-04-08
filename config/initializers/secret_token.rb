1f14eed147be699bc8e268974f5c4dcff010379d
Luyue <lzhao49@wisc.edu>
2014-04-07 20:56:39 -0500

    new rail project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TyphoonBlogger::Application.config.secret_key_base = 'ce15512fe03e14f0b6a9b09573ccb4cf0968233995c556b2d1b69bccd6664a7f527c6cd321ff0749a59dd9e89776a7d6318917203f80125600ead43f0a6d1835'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TyphoonBlogger::Application.config.session_store :cookie_store, key: '_Typhoon_Blogger_session'
