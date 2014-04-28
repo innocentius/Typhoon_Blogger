b85aa2eab9eee4c557d5c130f50c0e6b0628200a
Luyue <lzhao49@wisc.edu>
2014-04-28 15:02:01 -0500

    Add features between post and user

--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -1,10 +1,34 @@
-Given /^a user visit the home page$/ do
-  visit root_path
+Given /^there exist a user$/ do
+  @user = User.create(name: "Example User", email: "user@example.com")
 end
-When /^they click the link Users$/ do
+When /^they click link Users$/ do
   click_link "Users"
 end
-Then /^they should be redirect to the about page$/ do
+Then /^they should be directed to user page$/ do
   expect(page).to have_content("Listing users")
 end
-
+When /^they click the first link show$/ do
+  click_link('Show')
+end
+Then /^they should be directed to user 1 page$/ do
+  expect(page).to have_content("Name")
+  expect(page).to have_content("Email")
+end
+Then /^they should see post link$/ do
+  expect(page).to have_link("Post")
+end
+When /^they click link Post$/ do
+  click_link "Post"
+end
+Then /^they should be directed to post page$/ do
+  expect(page).to have_content("Listing posts")
+end
+Then /^they should also see home link$/ do
+  expect(page).to have_link("Home")
+end
+When /^they click link home$/ do
+  click_link "Home"
+end
+Then /^they should be redirected to home$/ do
+  expect(page).to have_content("StaticPages#home")
+end
